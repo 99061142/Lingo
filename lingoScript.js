4216f323707dc82b871190d18de9e767cabe4d7a
99061142 <99061142@mydavinci.nl>
2021-01-24 21:54:22 +0100

    Made it better to read

--- lingoScript.js
+++ lingoScript.js
@@ -1,18 +1,17 @@
-//Pakt een random lingo woord uit de array
-var lingoWord = Math.floor(Math.random() * words.length);
-//var lingoWordChooser = words[lingoWord];
-var lingoWordChooser = "water";
-var lingoWordChooserSplitter = lingoWordChooser.split('');
+//Pakt een lingo woord
+const lingoWordChooser = Math.floor(Math.random() * words.length);
+//Splitst het lingo woord in aparte buttons
+var lingoWordSplit = words[lingoWordChooser].split('');
 
 
 //Maakt de buttons aan
-for (i = 0; i < lingoWordChooser.length; i++){
-	var letters = document.createElement("button");
-	wordshowerbox.appendChild(letters);
+for (i = 0; i < lingoWordSplit.length; i++){ 
+	var buttons = document.createElement("button");
+	wordshowerbox.appendChild(buttons);
 	//De eerste letter wordt altijd weergeven
 	if(i == 0){
-		letters.innerHTML = lingoWordChooser[0]
-		letters.style.backgroundColor = "green"
+		buttons.innerHTML = lingoWordSplit[0]
+		buttons.style.backgroundColor = "green"
 	}
 }
 
@@ -21,28 +20,28 @@ for (i = 0; i < lingoWordChooser.length; i++){
 check.onclick = function(){
 	//Checkt en splitst het 5 letter woord die de gebruiker heeft geraden
 	var wordInput = document.getElementById("wordInput");
-	var ownWordChooserSplitter = wordInput.value.split('');
+	var guessedWord = wordInput.value.split('');
 
 	
 	//Als het geraden woord even lang is als het lingo woord mag de gebruiker verder
-	if(ownWordChooserSplitter.length == lingoWordChooser.length){
+	if(guessedWord.length == lingoWordSplit.length){
 		//Maakt een nieuwe rij met de lettters
-		for (i = 0; i < ownWordChooserSplitter.length; i++){
-			//Maakt een nieuwe rij met de lettters
-			var letters = document.createElement("button");
-			wordshowerbox.appendChild(letters);
-			letters.innerHTML = ownWordChooserSplitter[i]
+		for (i = 0; i < guessedWord.length; i++){
+			//Maakt een rij voor de buttons met het geraden woord
+			var buttons = document.createElement("button");
+			wordshowerbox.appendChild(buttons);
+			buttons.innerHTML = guessedWord[i]
 
 			//Als de letter van het geraden woord overeenkomt met de letter van het lingo woord 
-			if(lingoWordChooserSplitter[i] == ownWordChooserSplitter[i]){
-				letters.style.backgroundColor = "green"
+			if(lingoWordSplit[i] == guessedWord[i]){
+				buttons.style.backgroundColor = "green"
 			}
 			//Als de letter van het geraden woord niet op de juiste plek zit van het lingo woord 
-			else if(lingoWordChooserSplitter.indexOf(ownWordChooserSplitter[i]) > -1){
-				letters.style.backgroundColor = "yellow"
-				letters.style.borderRadius = "50%";
+			else if(lingoWordSplit.indexOf(guessedWord[i]) > -1){
+				buttons.style.backgroundColor = "yellow"
+				buttons.style.borderRadius = "50%";
 			}
 		}
 	}
 }
-console.log(lingoWordChooserSplitter)
+console.log(lingoWordSplit)
