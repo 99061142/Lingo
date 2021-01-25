6a63d27c4f0276529674d03c71929920373e85cb
99061142 <99061142@mydavinci.nl>
2021-01-25 14:57:00 +0100

    Update lingoScript.js
    You can only guess 5 times

--- lingoScript.js
+++ lingoScript.js
@@ -12,13 +12,14 @@ for (i = 0; i < lingoWordSplit.length; i++){
 	wordshowerbox.appendChild(buttons);
 	//De eerste letter wordt altijd weergeven
 	if(i == 0){
-		buttons.innerHTML = lingoWordSplit[0]
+		buttons.innerText = lingoWordSplit[0]
 		buttons.style.backgroundColor = "green"
 	}
 }
 
 
 //De functie start als je op "CHECK" klikt
+var checkPresser = 0;
 check.onclick = function(){
 	//Checkt en splitst het 5 letter woord die de gebruiker heeft geraden
 	var wordInput = document.getElementById("wordInput");
@@ -32,7 +33,7 @@ check.onclick = function(){
 			//Maakt een rij voor de buttons met het geraden woord
 			var buttons = document.createElement("button");
 			wordshowerbox.appendChild(buttons);
-			buttons.innerHTML = guessedWordSplit[i]
+			buttons.innerText = guessedWordSplit[i]
 
 			//Als de letter van het geraden woord overeenkomt met de letter van het lingo woord 
 			if(lingoWordSplit[i] == guessedWordSplit[i]){
@@ -48,9 +49,13 @@ check.onclick = function(){
 				lingoWordSplit[deleteLetter] = "";
 			}
 		}
+		//Je kan maximaal 5 keer gokken
+		checkPresser++
+		if(checkPresser == 5){
+			document.getElementById("check").disabled = true
+		}
 	}
 	//Maakt weer de array met het lingo woord aan
 	lingoWordSplit = words.split('');
-	console.log(lingoWordSplit)
 }
 console.log(lingoWordSplit)
