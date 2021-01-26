6db9c7e05ac41f54d8263d43f0a732403d40e5cd
99061142 <99061142@mydavinci.nl>
2021-01-26 09:59:18 +0100

    Done
    All of the code is made smaller + everything works

--- lingoScript.js
+++ lingoScript.js
@@ -1,25 +1,23 @@
 //Pakt een lingo woord
-//const lingoWordChooser = Math.floor(Math.random() * words.length);
-//Splitst het lingo woord in aparte buttons
-//var lingoWordSplit = words[lingoWordChooser].split('');
-var words = "water";
-var lingoWordSplit = words.split('');
+const lingoWordChooser = Math.floor(Math.random() * words.length);
+//Splitst het lingo woord in aparte letters
+var lingoWordSplit = words[lingoWordChooser].split('');
 
 
-//Maakt de buttons aan
-for (i = 0; i < lingoWordSplit.length; i++){ 
-	var buttons = document.createElement("button");
-	wordshowerbox.appendChild(buttons);
+//Maakt de buttonLetters aan
+for(i = 0; i < lingoWordSplit.length; i++){ 
+	var buttonLetters = document.createElement("button");
+	whiteDiv.appendChild(buttonLetters);
 	//De eerste letter wordt altijd weergeven
 	if(i == 0){
-		buttons.innerText = lingoWordSplit[0]
-		buttons.style.backgroundColor = "green"
+		buttonLetters.innerText = lingoWordSplit[0];
+		buttonLetters.style.backgroundColor = "green";
 	}
 }
 
 
-//De functie start als je op "CHECK" klikt
 var checkPresser = 0;
+//De functie start als je op "CHECK" klikt
 check.onclick = function(){
 	//Checkt en splitst het 5 letter woord die de gebruiker heeft geraden
 	var wordInput = document.getElementById("wordInput");
@@ -30,32 +28,31 @@ check.onclick = function(){
 	if(guessedWordSplit.length == lingoWordSplit.length){
 		//Maakt een nieuwe rij met de lettters
 		for (i = 0; i < guessedWordSplit.length; i++){
-			//Maakt een rij voor de buttons met het geraden woord
-			var buttons = document.createElement("button");
-			wordshowerbox.appendChild(buttons);
-			buttons.innerText = guessedWordSplit[i]
+			//Maakt een rij voor de letters met het geraden woord
+			var buttonLetters = document.createElement("button");
+			whiteDiv.appendChild(buttonLetters);
+			buttonLetters.innerHTML = guessedWordSplit[i];
 
 			//Als de letter van het geraden woord overeenkomt met de letter van het lingo woord 
 			if(lingoWordSplit[i] == guessedWordSplit[i]){
-				buttons.style.backgroundColor = "green"
-				lingoWordSplit[i] = ""
+				buttonLetters.style.backgroundColor = "green";
+				lingoWordSplit[i] = "";
 			}
 
 			//Als de letter van het geraden woord niet op de juiste plek zit van het lingo woord 
 			else if(lingoWordSplit.indexOf(guessedWordSplit[i]) > -1){
-				buttons.style.backgroundColor = "yellow"
-				buttons.style.borderRadius = "50%";
-				var deleteLetter = lingoWordSplit.indexOf(guessedWordSplit[i])
+				buttonLetters.style.backgroundColor = "yellow";
+				buttonLetters.style.borderRadius = "50%";
+				var deleteLetter = lingoWordSplit.indexOf(guessedWordSplit[i]);
 				lingoWordSplit[deleteLetter] = "";
 			}
 		}
-		//Je kan maximaal 5 keer gokken
-		checkPresser++
+		//Je kan maximaal 5 keer het woord gokken
+		checkPresser++;
 		if(checkPresser == 5){
-			document.getElementById("check").disabled = true
+			document.getElementById("check").disabled = true;
 		}
 	}
 	//Maakt weer de array met het lingo woord aan
-	lingoWordSplit = words.split('');
+	lingoWordSplit = words[lingoWordChooser].split('');
 }
-console.log(lingoWordSplit)
