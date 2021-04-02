5dc46fdbbe495320f597330342d0c3a023d47ab2
99061142 <99061142@mydavinci.nl>
2021-04-02 23:42:25 +0200

    JavaScript code veranderd
    De browser laat op het einde zien of je het wel, of niet hebt geraden, en wat het woord was.

--- lingoScript.js
+++ lingoScript.js
@@ -1,58 +1,84 @@
-//Pakt een lingo woord
+// Variables
 const lingoWordChooser = Math.floor(Math.random() * words.length);
-//Splitst het lingo woord in aparte letters
 var lingoWordSplit = words[lingoWordChooser].split('');
+var buttonLetters, text, wordShower, userwordShower;
+var correctCounter = checkPresser = 0;
 
 
-//Maakt de buttonLetters aan
+// Maakt de eerste rij aan voor de letters
 for(i = 0; i < lingoWordSplit.length; i++){ 
-	var buttonLetters = document.createElement("button");
+	buttonLetters = document.createElement('button');
 	whiteDiv.appendChild(buttonLetters);
-	//De eerste letter wordt altijd weergeven
+	// De eerste letter wordt altijd weergeven
 	if(i == 0){
 		buttonLetters.innerText = lingoWordSplit[0];
-		buttonLetters.style.backgroundColor = "green";
+		buttonLetters.style.backgroundColor = 'green';
 	}
 }
 
 
-var checkPresser = 0;
-//De functie start als je op "CHECK" klikt
+
+
 check.onclick = function(){
-	//Checkt en splitst het 5 letter woord die de gebruiker heeft geraden
-	var wordInput = document.getElementById("wordInput");
+	// Checkt en splitst het 5 letter woord die de gebruiker heeft geraden
+	var wordInput = document.getElementById('wordInput');
 	var guessedWordSplit = wordInput.value.split('');
 
 	
-	//Als het geraden woord even lang is als het lingo woord mag de gebruiker verder
+	// Als het geraden woord even lang is als het lingo woord mag de gebruiker verder
 	if(guessedWordSplit.length == lingoWordSplit.length){
-		//Maakt een nieuwe rij met de lettters
+		for(i = 0; i < lingoWordSplit.length; i++){ 
+			if(guessedWordSplit[i] === lingoWordSplit[i]){ correctCounter++; }
+		}
+		// Maakt een nieuwe rij met de lettters
 		for(i = 0; i < guessedWordSplit.length; i++){
-			//Maakt een rij voor de letters met het geraden woord
-			var buttonLetters = document.createElement("button");
+			// Maakt een rij voor de letters met het geraden woord
+			buttonLetters = document.createElement('button');
 			whiteDiv.appendChild(buttonLetters);
 			buttonLetters.innerHTML = guessedWordSplit[i];
 
-			//Als de letter van het geraden woord overeenkomt met de letter van het lingo woord 
+			// Als de letter van het geraden woord overeenkomt met de letter van het lingo woord
 			if(lingoWordSplit[i] == guessedWordSplit[i]){
-				buttonLetters.style.backgroundColor = "green";
-				lingoWordSplit[i] = "";
+				buttonLetters.style.backgroundColor = 'green';
+				lingoWordSplit[i] = '';
 			}
 
-			//Als de letter van het geraden woord niet op de juiste plek zit van het lingo woord 
+			//Als de letter van het geraden woord niet op de juiste plek zit van het lingo woord
 			else if(lingoWordSplit.indexOf(guessedWordSplit[i]) > -1){
-				buttonLetters.style.backgroundColor = "yellow";
-				buttonLetters.style.borderRadius = "50%";
+				buttonLetters.style.backgroundColor = 'yellow';
+				buttonLetters.style.borderRadius = '50%';
 				var deleteLetter = lingoWordSplit.indexOf(guessedWordSplit[i]);
-				lingoWordSplit[deleteLetter] = "";
+				lingoWordSplit[deleteLetter] = '';
 			}
 		}
-		//Je kan maximaal 5 keer het woord gokken
+		// Vult weer de lingo array met het woord
+		lingoWordSplit = words[lingoWordChooser].split('');
+
+
 		checkPresser++;
-		if(checkPresser == 5){
-			document.getElementById("check").disabled = true;
+		// Het bord wordt verwijderd als je 5 keer hebt geraden, of het woord goed hebt geraden
+		if(checkPresser == 5 || correctCounter == 5){
+			document.getElementById('gameboard').remove();
+			text = document.createElement('h1');
+			wordShower = document.createElement('h2');
+			userwordShower = document.createElement('h2');
+			document.body.appendChild(text);
+			document.body.appendChild(wordShower);
+			document.body.appendChild(userwordShower);
+
+
+			wordShower.innerHTML = 'Het woord was ' + lingoWordSplit.join('');
+			if(correctCounter == 5){ 
+				document.body.style.backgroundColor = 'green'; 
+				text.innerHTML = 'Je hebt gewonnnen';
+				userwordShower.innerHTML = 'Jou woord was ' + guessedWordSplit.join('');
+			}else{ 
+				document.body.style.backgroundColor = 'red'; 
+				text.innerHTML = 'Je hebt verloren';
+				userwordShower.innerHTML = 'Jou laatste gok was ' + guessedWordSplit.join('');
+			}	
+			checkPresser = 0;
 		}
+		correctCounter = 0;
 	}
-	//Maakt weer de array met het lingo woord aan
-	lingoWordSplit = words[lingoWordChooser].split('');
 }
