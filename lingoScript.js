deba75f32ca5ac97c8b914debad1a5e898b24042
99061142 <99061142@mydavinci.nl>
2021-12-17 23:38:41 +0100

    Changed most of the code to make it more readable
    * This code does not show the end screen, this is only the functionality to guess the word

--- lingoScript.js
+++ lingoScript.js
@@ -1,84 +1,94 @@
-// Variables
-const lingoWordChooser = Math.floor(Math.random() * words.length);
-var lingoWordSplit = words[lingoWordChooser].split('');
-var buttonLetters, text, wordShower, userwordShower;
-var correctCounter = checkPresser = 0;
+// Random word
+const random = Math.floor(Math.random() * words.length);
+const word = words[random].split('');
+var not_guessed_letters = word // Check which letters the user has guessed correctly
 
 
-// Maakt de eerste rij aan voor de letters
-for(i = 0; i < lingoWordSplit.length; i++){ 
-	buttonLetters = document.createElement('button');
-	whiteDiv.appendChild(buttonLetters);
-	// De eerste letter wordt altijd weergeven
-	if(i == 0){
-		buttonLetters.innerText = lingoWordSplit[0];
-		buttonLetters.style.backgroundColor = 'green';
-	}
+// Show the user the letters he guessed correctly / are in the word the user need to guess
+function check_word(guessed_word = null){
+    // Make a new row
+    for(i = 0; i < word.length; i++){
+        // Make a new box
+        letter_box = document.createElement('button');
+        whiteDiv.appendChild(letter_box);
+
+        // When the game starts only the first letter gets shown
+        if(!guessed_word && i == 0){
+            // Change box styling
+            letter_box.innerText = word[i];
+            letter_box.style.backgroundColor = "green";
+        }
+
+        // If the game has begun, and the user is guessing the words
+        else if(guessed_word){
+            letter_box.innerText = guessed_word[i]; // Add the letter to the box
+
+
+            // If the letter is in the correct position
+            if(word[i] == guessed_word[i]){
+                letter_box.style.backgroundColor = "green"; // Change box styling
+                not_guessed_letters[i] = ''; // Delete the guessed position
+            }
+
+            // If the letter is not in the correct position but it is in the word the user must guess
+            else if(not_guessed_letters.indexOf(guessed_word[i]) > -1){
+                // Change box styling
+                letter_box.style.backgroundColor = 'yellow';
+                letter_box.style.borderRadius = '50%';
+
+                var correct_position = word.indexOf(guessed_word[i]); // When the letter is in another position
+                not_guessed_letters[correct_position] = ''; // Delete the position where the guessed letter is
+            }
+        }
+    }
+    not_guessed_letters = word // Reset the letters the user need to guess
+}
+check_word()
+
+
+// Check if the guessed word is a valid option
+function word_validation(guessed_word){
+    var validation = true;
+
+    if(guessed_word.length == word.length){
+        const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Letter options
+
+        // Check if the guessed letter is an option
+        for(i = 0; i < word.length; i++){ 
+            if(!letters.includes(guessed_word[i])){
+                validation = false;
+            }
+        }
+    }
+    
+    else{
+        validation = false;
+    }
+
+    return validation;
 }
 
 
+// Error message if the word is not a valid option
+function error_message(){
+    document.getElementById('guessed_word').value = '' // Delete the input of the user
+    document.getElementById('guessed_word').placeholder = `Kies een woord met ${word.length} letters` // Show the reason
+}
 
 
+// If the user clicks submit
 check.onclick = function(){
-	// Checkt en splitst het 5 letter woord die de gebruiker heeft geraden
-	var wordInput = document.getElementById('wordInput');
-	var guessedWordSplit = wordInput.value.split('');
+    const guessed_word = document.getElementById('guessed_word').value.split('') // Guessed word
 
-	
-	// Als het geraden woord even lang is als het lingo woord mag de gebruiker verder
-	if(guessedWordSplit.length == lingoWordSplit.length){
-		for(i = 0; i < lingoWordSplit.length; i++){ 
-			if(guessedWordSplit[i] === lingoWordSplit[i]){ correctCounter++; }
-		}
-		// Maakt een nieuwe rij met de lettters
-		for(i = 0; i < guessedWordSplit.length; i++){
-			// Maakt een rij voor de letters met het geraden woord
-			buttonLetters = document.createElement('button');
-			whiteDiv.appendChild(buttonLetters);
-			buttonLetters.innerHTML = guessedWordSplit[i];
+	const validation = word_validation(guessed_word) // Check if the guessed word is an option
 
-			// Als de letter van het geraden woord overeenkomt met de letter van het lingo woord
-			if(lingoWordSplit[i] == guessedWordSplit[i]){
-				buttonLetters.style.backgroundColor = 'green';
-				lingoWordSplit[i] = '';
-			}
-
-			//Als de letter van het geraden woord niet op de juiste plek zit van het lingo woord
-			else if(lingoWordSplit.indexOf(guessedWordSplit[i]) > -1){
-				buttonLetters.style.backgroundColor = 'yellow';
-				buttonLetters.style.borderRadius = '50%';
-				var deleteLetter = lingoWordSplit.indexOf(guessedWordSplit[i]);
-				lingoWordSplit[deleteLetter] = '';
-			}
-		}
-		// Vult weer de lingo array met het woord
-		lingoWordSplit = words[lingoWordChooser].split('');
-
-
-		checkPresser++;
-		// Het bord wordt verwijderd als je 5 keer hebt geraden, of het woord goed hebt geraden
-		if(checkPresser == 5 || correctCounter == 5){
-			document.getElementById('gameboard').remove();
-			text = document.createElement('h1');
-			wordShower = document.createElement('h2');
-			userwordShower = document.createElement('h2');
-			document.body.appendChild(text);
-			document.body.appendChild(wordShower);
-			document.body.appendChild(userwordShower);
-
-
-			wordShower.innerHTML = 'Het woord was ' + lingoWordSplit.join('');
-			if(correctCounter == 5){ 
-				document.body.style.backgroundColor = 'green'; 
-				text.innerHTML = 'Je hebt gewonnnen';
-				userwordShower.innerHTML = 'Jou woord was ' + guessedWordSplit.join('');
-			}else{ 
-				document.body.style.backgroundColor = 'red'; 
-				text.innerHTML = 'Je hebt verloren';
-				userwordShower.innerHTML = 'Jou laatste gok was ' + guessedWordSplit.join('');
-			}	
-			checkPresser = 0;
-		}
-		correctCounter = 0;
-	}
+    // If its a valid option
+    if(validation){
+        check_word(guessed_word) // Show the correctly guessed letters
+    }
+    
+    // If its not a valid option
+    else{
+        error_message() // Show the error message
+    }
 }
