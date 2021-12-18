824ee5ed577b5a72aa02ec30059b23f5ae6963df
99061142 <99061142@mydavinci.nl>
2021-12-18 16:59:45 +0100

    Added the end screen

--- code.js
+++ code.js
@@ -2,12 +2,16 @@
 const random = Math.floor(Math.random() * words.length);
 var word = words[random].split('');
 
+var chances = 0;
+
 
 // Show the user the letters he guessed correctly / are in the word the user need to guess
 function check_word(guessed_word = null){
+    correctly_guessed = false;
+
     // Make a new row
 	guessed_word_box = document.createElement('div');
-	guessed_word_box.className = 'guessed_word_box'
+	guessed_word_box.className = 'guessed_word_box';
 
 	box_div.appendChild(guessed_word_box);
 
@@ -48,10 +52,17 @@ function check_word(guessed_word = null){
 
     // Reset the letters if the user guessed a word
     if(guessed_word){
-        word = words[random].split('');
+        // Check if the word is correctly guessed 
+        if(word.every( v => v === word[0])){
+            correctly_guessed = true;
+        }
+        word = words[random].split(''); // Reset the word
+
+        return correctly_guessed; // Return if the word is correctly guessed
     }
 }
-check_word()
+console.log(word)
+check_word();
 
 
 // Check if the guessed word is a valid option
@@ -79,24 +90,52 @@ function word_validation(guessed_word){
 
 // Error message if the word is not a valid option
 function error_message(){
-    document.getElementById('guessed_word').value = '' // Delete the input of the user
-    document.getElementById('guessed_word').placeholder = `Kies een woord met ${word.length} letters` // Show the reason
+    document.getElementById('guessed_word').value = ''; // Delete the input of the user
+    document.getElementById('guessed_word').placeholder = `Kies een woord met ${word.length} letters`; // Show the reason
+}
+
+
+// End screen
+function end_screen(correctly_guessed){
+    document.getElementById('container').remove(); // Delete the game
+
+    document.body.style.backgroundColor = correctly_guessed ? "LightGreen" : "Tomato"; // Change background color
+
+
+    won_or_lost = correctly_guessed ? "Gewonnen" : "Verloren"; // Information if the user won or lost
+
+    // Make text to show the end text
+    end_text = document.createElement('h1');
+    end_text.innerHTML = `U heeft ${won_or_lost}, het woord was '${word.join('')}'.`;
+
+    // Show the user how many chances he have made
+    if(correctly_guessed){
+        end_text.innerHTML += ` Het totaal aantal beurten voordat u het woord wist was ${chances}/5`;
+    }
+
+    document.body.appendChild(end_text); // Add the text to the body
 }
 
 
 // If the user clicks submit
 check.onclick = function(){
-    const guessed_word = document.getElementById('guessed_word').value.split('') // Guessed word
+    const guessed_word = document.getElementById('guessed_word').value.split(''); // Guessed word
 
-	const validation = word_validation(guessed_word) // Check if the guessed word is an option
+	const validation = word_validation(guessed_word); // Check if the guessed word is an option
 
     // If its a valid option
     if(validation){
-        check_word(guessed_word) // Show the correctly guessed letters
+        correctly_guessed = check_word(guessed_word); // Show the correctly guessed letters
+        
+        chances++ // Add 1 to the chances the user made (5 max)
+
+        if(correctly_guessed || chances == 5){
+            end_screen(correctly_guessed);
+        }
     }
     
     // If its not a valid option
     else{
-        error_message() // Show the error message
+        error_message(); // Show the error message
     }
 }
